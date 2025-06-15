a9937da2828833ed85c10b994f4e9053d08f3012
Oksana Kryschuk <kovart.public@gmail.com>
2025-06-15 10:01:13 +0000

    test: add placeholder test

---
+++ test/placeholder.test.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe('Placeholder test to ensure test suite integrity', function () {
+  it('should run a trivial assertion', async function () {
+    // simple sanity check
+    expect(true).to.equal(true);
+  });
+});
